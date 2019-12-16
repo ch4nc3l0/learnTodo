9dd3296cc9f68bddc0e700856092d6c477754e2f
chance <ch4nc3l0@gmail.com>
2019-12-15 23:19:18 -0500

    Add Todo interface

---
+++ Services/ITodo.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCoreTodo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Services // This interface will be interacting with the database || Not being handled by the controller
+{
+    interface ITodo
+    {
+        Task<Todo> GetTodo(); // Defining an async method to get todos from the database
+    }
+}
