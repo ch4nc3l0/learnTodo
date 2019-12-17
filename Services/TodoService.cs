082c6da36e88f26cc02a4c70b81acbbf5399b92b
chance <ch4nc3l0@gmail.com>
2019-12-16 22:54:58 -0500

    Add TodoService to use the database

---
+++ Services/TodoService.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Services
+{
+    public class TodoService : ITodoService
+    {
+        // Declare a var
+        private readonly ApplicationDbContext _context;
+
+        // Set equal to context
+        public TodoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        // Return var with data
+        public async Task<Todo[]> GetTodo()
+        {
+            return await _context.Todos
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,7 @@ namespace AspNetCoreTodo
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
            services.AddRazorPages();
-            services.AddSingleton<ITodoService, FakeTodoItemService>();
+            services.AddScoped<ITodoService, TodoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
